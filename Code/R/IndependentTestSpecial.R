068c9f7da3fb1763e428a24edc6150db23fa1d69
Saifur Rahman <saifur80@gmail.com>
2017-08-17 07:22:37 +0600

    Regression threshold tuning

---
+++ Code/R/IndependentTestSpecial.R
@@ -0,0 +1,90 @@
+library(e1071)
+library(ROCR)
+library(randomForest)
+
+source('featurefiltering.R');
+
+timestamp();
+
+#set.seed(10);
+
+svmC            = 10;
+maxFeatureCount = 2500; # 2650, 2500
+
+balancing = "_SMOTED";
+fScheme   = "_comb";
+
+# File names #
+fileNameSuffix = paste(fScheme, balancing, ".rds", sep = "");
+
+rankedFeaturesFile  = "rankedFeatures.rds";
+#rankedFeaturesFile = paste("ff"            , fileNameSuffix, sep = "");
+featureFile        = paste("featurized"    , fileNameSuffix, sep = "");
+testFeatureFile    = paste("testFeaturized", fScheme,".rds", sep = "");
+svmFile            = paste("svm"           , fileNameSuffix, sep = "");
+
+cat(as.character(Sys.time()),">> Reading training set features from", featureFile, "...\n");
+features = readRDS(featureFile);
+cat(as.character(Sys.time()),">> Done\n");
+
+cat(as.character(Sys.time()),">> Reading test set features from", testFeatureFile, "...\n");
+testFeatures = readRDS(testFeatureFile);
+cat(as.character(Sys.time()),">> Done\n");
+
+cat(as.character(Sys.time()),">> Reading feature ranking from", rankedFeaturesFile, "...\n");
+rankedFeatures = readRDS(rankedFeaturesFile);
+cat(as.character(Sys.time()),">> Done\n");
+
+# random shuffle of features
+features <- features[sample(nrow(features)),]
+
+# For regression study, we need to 'unfactor' the dependent var.
+#
+
+# Cis-Golgi becomes 1 and Trans-Golgi becomes 2.
+# But we want Cis-Golgi (positive class) to be 1 and Trans-Golgi to be 0
+features$protection = 2 - as.numeric(features$protection);
+testFeatures$protection  = 2 - as.numeric(testFeatures$protection);
+
+cat(as.character(Sys.time()),">> Entering independent validation ...\n");
+
+trainingSet = featurefiltering(features, rankedFeatures, maxFeatureCount);
+testSet = featurefiltering(testFeatures, rankedFeatures, maxFeatureCount);
+
+model = svm(protection ~ ., trainingSet, cost = svmC, kernel="linear");
+pred = predict(model, testSet);
+
+# perform regression based perf. measurements
+
+# Find optimal threshold based on accuracy
+# Also find the AUCROC
+predAndTruth = prediction(pred, testSet$protection);
+auc  = ROCR::performance(predAndTruth,"auc")@y.values[[1]];
+
+cat(as.character(Sys.time()),">> Recording performance in results.csv ...\n");
+itData = data.frame(matrix(ncol = 4, nrow = 0))
+for (threshold in seq(from=0.30, to=0.70, by=0.05)) {
+  
+  predAndTruth = prediction(as.numeric(pred >= threshold), as.factor(testSet$protection));
+  acc  = unlist(ROCR::performance(predAndTruth,"acc")@y.values)[2];
+  sens = unlist(ROCR::performance(predAndTruth,"sens")@y.values)[2];
+  spec = unlist(ROCR::performance(predAndTruth,"spec")@y.values)[2];
+  mcc = unlist(ROCR::performance(predAndTruth,"mat")@y.values)[2];
+  
+  itData = rbind(itData, c(threshold, acc, sens, spec, mcc));
+}
+colnames(itData) = c("Threshold", "Accuracy", "Sensitivity", "Specificity", "MCC");
+write.csv(t(itData), "results.csv");
+cat(as.character(Sys.time()),">> Done.\n");
+
+cat(as.character(Sys.time()),">> Calculating enrichment ...\n");
+sortOrder = order(-pred);
+
+for (rank in c(2,5,10,25)) {
+  
+  topRanked = round(rank * length(testSet[,1]) / 100, 0);
+  num = sum(testSet[sortOrder[1:topRanked],"protection"])/topRanked;
+  den = sum(testSet$protection) / length(testSet[,1]);
+  cat(round(num / den, 1), "\n")
+}
+
